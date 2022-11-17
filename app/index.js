38830bdb4f0ba54710f3388bce49e6a8b24a7be7
Marc Templeton <marcte@kainos.com>
2022-11-17 11:10:37 +0000

    testing

--- app/index.js
+++ app/index.js
@@ -3,6 +3,12 @@ const server = require('./server')
 const init = async () => {
   await server.start()
   console.log('Server running on %s', server.info.uri)
+
+  console.log('caching...')
+  server.app.cache.set('willThisWork', 'dwdssfsf')
+  console.log('set')
+  const res = server.app.cache.get('willThisWork')
+  console.log('cache res', res)
 }
 
 process.on('unhandledRejection', (err) => {
