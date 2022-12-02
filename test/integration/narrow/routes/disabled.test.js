85678c2b38d568d4a34f01a3f9550cba40cde356
John Watson <johnwatson484@hotmail.com>
2022-12-02 13:44:51 +0000

    Add tests

---
+++ test/integration/narrow/routes/disabled.test.js
@@ -0,0 +1,46 @@
+const config = require('../../../../app/config')
+config.endpointEnabled = false
+const createServer = require('../../../../app/server')
+let server
+
+describe('Disabled endpoint', () => {
+  beforeEach(async () => {
+    server = await createServer()
+    await server.initialize()
+  })
+
+  afterEach(async () => {
+    await server.stop()
+  })
+
+  test('GET /statement/{version}/{filename} route returns 503 if endpoint disabled', async () => {
+    const options = {
+      method: 'GET',
+      url: '/statement/v1/FFC_PaymentStatement_SFI_2022_1234567890_2022080515301012.pdf'
+    }
+
+    const response = await server.inject(options)
+    expect(response.statusCode).toBe(503)
+    expect(response.payload).toBe('Service is intentionally disabled in this environment')
+  })
+
+  test('GET /healthy route returns 200 if endpoint disabled', async () => {
+    const options = {
+      method: 'GET',
+      url: '/healthy'
+    }
+
+    const response = await server.inject(options)
+    expect(response.statusCode).toBe(200)
+  })
+
+  test('GET /healthz route returns 200 if endpoint disabled', async () => {
+    const options = {
+      method: 'GET',
+      url: '/healthz'
+    }
+
+    const response = await server.inject(options)
+    expect(response.statusCode).toBe(200)
+  })
+})
