6c956783f5796e04f1fd0ef6a59a910f2c70ebb7
Simon Dunn <simondunn90@gmail.com>
2022-12-07 14:21:33 +0000

    tests

--- test/unit/stream-to-buffer.test.js
+++ test/unit/stream-to-buffer.test.js
@@ -18,12 +18,57 @@ describe('readable stream contents is converted to an int Buffer', () => {
     jest.resetAllMocks()
   })
 
-  test('should return 1', async () => {
-    const result = streamToBuffer(readableStream)
-    expect(result).toBe(1) // what should this actually return, probs a Buffer of everything in streamData
+  test('should return an instance of a Buffer when valid StreamData provided', async () => {
+    const result = await streamToBuffer(readableStream)
+    expect(result).toBeInstanceOf(Buffer) // what should this actually return, probs a Buffer of everything in streamData
   })
 
-  // what forms can readableStream take? (check the docs)
+  // test('should return a Buffer of streamData', async () => {
+  //   const result = await streamToBuffer(readableStream)
+  //   expect(result).toBe(Buffer.from(readableStream))
+  //   // How can I check this returns a Buffer of streamData without repeating the functionality of the stream-to-buffer function
+  // })
+
+  test('should return an instance of a Buffer when readableFlowing is set to null', async () => {
+    readableStream.readableFlowing = null
+    const result = await streamToBuffer(readableStream)
+    expect(result).toBeInstanceOf(Buffer)
+  })
+
+  // test('should return an instance of a Buffer when readableFlowing is set to true', async () => {
+  //   readableStream.readableFlowing = true
+  //   const result = await streamToBuffer(readableStream)
+  //   expect(result).toBeInstanceOf(Buffer)
+  // })
+
+  test('should reject to a TypeError when streamToBuffer is called with an array', async () => {
+    readableStream = []
+    try {
+      await streamToBuffer(readableStream)
+    } catch (err) {
+      expect(err).toBeInstanceOf(TypeError)
+    }
+  })
+
+  test('should reject to a TypeError when streamToBuffer is called with a string', async () => {
+    readableStream = 'string'
+    try {
+      await streamToBuffer(readableStream)
+    } catch (err) {
+      expect(err).toBeInstanceOf(TypeError)
+    }
+  })
+
+  test('should reject to a TypeError when streamToBuffer is called with a object', async () => {
+    readableStream = { key: 'value' }
+    try {
+      await streamToBuffer(readableStream)
+    } catch (err) {
+      expect(err).toBeInstanceOf(TypeError)
+    }
+  })
+
+  // what forms can readableStream take? (check the docs) 'flowing' and 'paused'? or readableFlowing true, false, null
   // based off this, what happens if readableStream is in each of these states?
   // i.e. what if readableStream is empty? what if we add to readableStream partway through?
   // what if this function gets a wrong input type, i.e. [], {}, 'stream', etc
