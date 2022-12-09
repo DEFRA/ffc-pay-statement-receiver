4ad0fd135f707de7ac17577689b369d7ee7a4630
Marc Templeton <marcte@kainos.com>
2022-12-09 09:21:36 +0000

    making pattern error message more readable

--- app/routes/schemas/filename.js
+++ app/routes/schemas/filename.js
@@ -5,7 +5,7 @@ const filenameRegex = require('../../constants/filename-regex')
 module.exports = {
   filename: Joi.string().pattern(filenameRegex).required()
     .messages({
-      'string.pattern.base': `Filename does not match the regular expression pattern of: ${filenameRegex}.`,
+      'string.pattern.base': 'Filename must end in .pdf.',
       'any.required': 'A filename must be provided.'
     })
 }
--- app/routes/schemas/version.js
+++ app/routes/schemas/version.js
@@ -2,5 +2,8 @@ const Joi = require('joi')
 
 module.exports = {
   version: Joi.string().required()
-    .messages({ 'string.base': 'Version must be provided as a string' })
+    .messages({
+      'string.base': 'Version must be a string.',
+      'any.required': 'A version must be provided.'
+    })
 }
